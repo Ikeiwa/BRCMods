e60d4ccb1686d5bebc4f5a2d2b165f2a7f691b13
ikeiwa <evann.delaunay@mail.com>
2023-08-24 08:41:26 +0200

    Added custom unity SDK for my mods

---
+++ BRCCustomModel/Patches.cs
@@ -0,0 +1,82 @@
+﻿using HarmonyLib;
+using Reptile;
+using UnityEngine;
+using System.Reflection;
+using System.Collections;
+using System;
+
+namespace BRCCustomModel
+{
+    internal class Patches
+    {
+        /*[HarmonyPatch(typeof(CharacterLoader), "AddCharacterFBX")]
+        static class Patch_CharacterLoader_AddCharacterFBX
+        {
+            [HarmonyPrefix]
+            static bool Prefix(Characters character, GameObject fbxAsset, ref GameObject[] ___loadedCharacterFbxAssets)
+            {
+                if (Plugin.customModelBundles.ContainsKey(character))
+                {
+                    GameObject newfbxAsset = Plugin.customModelAssets[character].fbx;
+
+                    ___loadedCharacterFbxAssets[(int)character] = newfbxAsset;
+
+                    return false;
+                }
+
+                return true;
+            }
+        }*/
+
+        [HarmonyPatch(typeof(CharacterConstructor))]
+        [HarmonyPatch(nameof(CharacterConstructor.CreateCharacterFbx))]
+        class Patch_CharacterConstructor_CreateCharacterFbx
+        {
+            static GameObject Postfix(GameObject returnValue, Characters character, CharacterConstructor __instance)
+            {
+                if (returnValue == null)
+                    return returnValue;
+
+                if (Plugin.customModelAssets.ContainsKey(character))
+                {
+                    GameObject newFbxAsset = Plugin.customModelAssets[character].fbx;
+
+                    if (newFbxAsset == null)
+                        return returnValue;
+
+                    return newFbxAsset;
+                }
+                else
+                    return returnValue;
+            }
+        }
+
+        [HarmonyPatch(typeof(CharacterConstructor))]
+        [HarmonyPatch(nameof(CharacterConstructor.CreateCharacterMaterial))]
+        class Patch_CharacterConstructor_CreateCharacterMaterial
+        {
+            static Material Postfix(Material returnValue, Characters character, int outfit, CharacterConstructor __instance)
+            {
+                if(returnValue == null)
+                    return returnValue;
+
+                if (Plugin.customModelAssets.ContainsKey(character))
+                {
+                    Material newmaterialAsset = null;
+
+                    if (outfit >= 0 && outfit < 4)
+                        newmaterialAsset = Plugin.customModelAssets[character].skins[outfit];
+
+                    if(newmaterialAsset == null)
+                        return returnValue;
+
+                    newmaterialAsset.shader = returnValue.shader;
+
+                    return newmaterialAsset;
+                }
+                else
+                    return returnValue;
+            }
+        }
+    }
+}
--- BRCCustomModel/Plugin.cs
+++ BRCCustomModel/Plugin.cs
@@ -1,14 +1,77 @@
 ﻿using BepInEx;
+using System.IO;
+using System;
+using HarmonyLib;
+using UnityEngine;
+using System.Collections.Generic;
+using System.Linq;
+using Reptile;
+using UnityEngine.TextCore.Text;
 
 namespace BRCCustomModel
 {
-    [BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+    public struct ModelAssets
+    {
+        public GameObject fbx;
+        public Material[] skins;
+    }
+
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
     public class Plugin : BaseUnityPlugin
     {
+        public static string playerModelsPath => Path.Combine(Environment.CurrentDirectory, "PlayerModels");
+
+        public static Dictionary<Characters, AssetBundle> customModelBundles;
+        public static Dictionary<Characters, ModelAssets> customModelAssets;
+
         private void Awake()
         {
+            if (!Directory.Exists(playerModelsPath))
+                Directory.CreateDirectory(playerModelsPath);
+
+            RefreshBundlePaths();
+
+            var harmony = new Harmony(PluginInfo.PLUGIN_GUID + ".patch");
+            harmony.PatchAll();
+
             // Plugin startup logic
-            Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+        }
+
+        private void RefreshBundlePaths()
+        {
+            customModelBundles = new Dictionary<Characters, AssetBundle>();
+            customModelAssets = new Dictionary<Characters, ModelAssets>();
+
+            string[] files = Directory.GetFiles(playerModelsPath, "*.brc", SearchOption.AllDirectories);
+
+            foreach ( string file in files )
+            {
+                if(Enum.TryParse(Path.GetFileNameWithoutExtension(file),out Characters bundleCharacter) && !customModelBundles.ContainsKey(bundleCharacter))
+                {
+                    Logger.LogInfo("Found avatar for "+ bundleCharacter.ToString() + " : " + file);
+
+                    AssetBundle bundle = AssetBundle.LoadFromFile(file);
+
+                    customModelBundles.Add(bundleCharacter, bundle);
+
+                    Material[] skins = new Material[]
+                    {
+                        bundle.LoadAsset<Material>(bundleCharacter.ToString() + "Mat0"),
+                        bundle.LoadAsset<Material>(bundleCharacter.ToString() + "Mat1"),
+                        bundle.LoadAsset<Material>(bundleCharacter.ToString() + "Mat2"),
+                        bundle.LoadAsset<Material>(bundleCharacter.ToString() + "Mat3"),
+                    };
+
+                    ModelAssets modelAssets = new ModelAssets
+                    {
+                        fbx = bundle.LoadAsset<GameObject>(bundleCharacter.ToString()),
+                        skins = skins,
+                    };
+
+                    customModelAssets.Add(bundleCharacter, modelAssets);
+                }
+            }
         }
     }
 }
